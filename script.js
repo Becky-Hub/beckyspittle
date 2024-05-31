1a0f1981ee8829870ee0679f9a8e6939d622347e
B-e-c-k-y <119670471+B-e-c-k-y@users.noreply.github.com>
2024-05-31 16:58:28 +0100

    structure commit

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('nav ul li a');
     const sections = document.querySelectorAll('section');
+    const changingText = document.getElementById('changing-text');
+    const textArray = [
+        " experience", 
+        " research", 
+        " philosophy",  
+        " interests"
+    ];
+    let textIndex = 0;
+
+    function changeText() {
+        textIndex = (textIndex + 1) % textArray.length;
+        changingText.textContent = textArray[textIndex];
+    }
+
+    setInterval(changeText, 2000);
 
     links.forEach(link => {
         link.addEventListener('click', (e) => {
@@ -12,5 +27,21 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             });
         });
+    });
+
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    const navLinks = document.getElementById('nav-links');
+    const closeMenu = document.getElementById('close-menu');
+
+    hamburgerMenu.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+
+    closeMenu.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+
+    document.getElementById('contact-me-button').addEventListener('click', () => {
+        window.location.href = '#contact';
     });
 });

98c731f30168986b17d97eb68559da8fba0ce234
Becky-Hub <becky.spittle@outlook.com>
2024-08-07 13:17:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('nav ul li a, .mobile-nav a');
     const changingText = document.getElementById('changing-text');
     const contactMeButton = document.getElementById('contact-me-button');
+    const hamburgerMenu = document.getElementById('hamburger-menu');
+    const mobileNav = document.getElementById('mobile-nav');
     const textArray = [
         " experience", 
         " research", 
@@ -22,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
     links.forEach(link => {
         link.addEventListener('click', (e) => {
             // Check if the link is a hash link for smooth scrolling
-            if (link.getAttribute('href').startsWith('#')) {
+            if (link.getAttribute('href') && link.getAttribute('href').startsWith('#')) {
                 e.preventDefault();
                 const targetId = link.getAttribute('href').substring(1);
                 const targetSection = document.getElementById(targetId);
@@ -38,5 +40,32 @@ document.addEventListener('DOMContentLoaded', function() {
         contactMeButton.addEventListener('click', () => {
             window.location.href = 'index.html#contact';
         });
+    }
+
+    hamburgerMenu.addEventListener('click', () => {
+        mobileNav.classList.toggle('open');
+        if (mobileNav.classList.contains('open')) {
+            hamburgerMenu.innerHTML = '<i class="fas fa-times"></i>';
+        } else {
+            hamburgerMenu.innerHTML = '<i class="fas fa-bars"></i>';
+        }
+    });
+
+    // Hide hamburger menu on larger screens
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 768) {
+            hamburgerMenu.style.display = 'none';
+            mobileNav.classList.remove('open');
+            hamburgerMenu.innerHTML = '<i class="fas fa-bars"></i>';
+        } else {
+            hamburgerMenu.style.display = 'flex';
+        }
+    });
+    
+    // Initial check
+    if (window.innerWidth > 768) {
+        hamburgerMenu.style.display = 'none';
+    } else {
+        hamburgerMenu.style.display = 'flex';
     }
 });

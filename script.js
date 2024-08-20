e6d0bed3132d4aa26c91ef539c7adda232bb5865
Becky-Hub <becky.spittle@outlook.com>
2024-08-20 18:00:51 +0100

    resolving merge confilcts

--- script.js
+++ script.js
@@ -2,11 +2,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('nav ul li a, .mobile-nav a');
     const changingText = document.getElementById('changing-text');
     const contactMeButton = document.getElementById('contact-me-button');
+<<<<<<< HEAD
+=======
+    const viewResumeBtn = document.getElementById('view-resume-btn');
+    const pdfModal = document.getElementById('pdf-modal');
+    const pdfCloseBtn = document.querySelector('.pdf-close-btn');
+    const body = document.body;
+>>>>>>> 5733b86 (initial commit from surface)
     const hamburgerMenu = document.getElementById('hamburger-menu');
     const mobileNav = document.getElementById('mobile-nav');
     const alpacaOverlay = document.getElementById('alpaca-overlay');
     const closeBtn = document.querySelector('.close-btn');
     const alpacaLink = document.querySelector('a[href="#alpacas"]');
+<<<<<<< HEAD
+=======
+    const logoLink = document.querySelector('.name a'); // "Becky Spittle" logo link
+    const contactLink = document.querySelector('nav ul li a[href="#contact"]');
+    const footer = document.querySelector('footer');
+>>>>>>> 5733b86 (initial commit from surface)
     const textArray = [
         " experience", 
         " research", 
@@ -15,6 +28,10 @@ document.addEventListener('DOMContentLoaded', function() {
     ];
     let textIndex = 0;
 
+<<<<<<< HEAD
+=======
+    // Change text function for introduction
+>>>>>>> 5733b86 (initial commit from surface)
     function changeText() {
         textIndex = (textIndex + 1) % textArray.length;
         changingText.textContent = textArray[textIndex];
@@ -24,9 +41,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const offset = 70; // Adjust this value based on the height of your header
     
+<<<<<<< HEAD
     links.forEach(link => {
         link.addEventListener('click', (e) => {
             // Check if the link is a hash link for smooth scrolling
+=======
+    // Smooth scroll for navigation links
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+>>>>>>> 5733b86 (initial commit from surface)
             if (link.getAttribute('href') && link.getAttribute('href').startsWith('#')) {
                 e.preventDefault();
                 const targetId = link.getAttribute('href').substring(1);
@@ -35,29 +58,61 @@ document.addEventListener('DOMContentLoaded', function() {
                     const targetPosition = targetSection.getBoundingClientRect().top + window.pageYOffset - offset;
                     window.scrollTo({ top: targetPosition, behavior: 'smooth' });
                 }
+<<<<<<< HEAD
                 // Close the mobile nav after clicking a link
+=======
+>>>>>>> 5733b86 (initial commit from surface)
                 if (mobileNav.classList.contains('open')) {
                     mobileNav.classList.add('close');
                     setTimeout(() => {
                         mobileNav.classList.remove('open');
                         mobileNav.classList.remove('close');
+<<<<<<< HEAD
                     }, 300); // Match this duration to your CSS transition duration
+=======
+                    }, 300);
+>>>>>>> 5733b86 (initial commit from surface)
                     hamburgerMenu.innerHTML = '<i class="fas fa-bars"></i>';
                 }
             }
         });
     });
 
+<<<<<<< HEAD
+=======
+    // Scroll to the top of the homepage when the logo is clicked
+    logoLink.addEventListener('click', (e) => {
+        // Check if the current page is the homepage
+        if (window.location.pathname === '/' || window.location.pathname === '/index.html') {
+            e.preventDefault();
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
+    });
+
+    // Open Alpaca overlay
+>>>>>>> 5733b86 (initial commit from surface)
     if (alpacaLink) {
         alpacaLink.addEventListener('click', function(event) {
             event.preventDefault();
             alpacaOverlay.style.display = 'flex';
+<<<<<<< HEAD
         });
     }
 
     if (closeBtn) {
         closeBtn.addEventListener('click', function() {
             alpacaOverlay.style.display = 'none';
+=======
+            body.classList.add('modal-open'); // Disable body scroll
+        });
+    }
+
+    // Close Alpaca overlay
+    if (closeBtn) {
+        closeBtn.addEventListener('click', function() {
+            alpacaOverlay.style.display = 'none';
+            body.classList.remove('modal-open'); // Enable body scroll
+>>>>>>> 5733b86 (initial commit from surface)
         });
     }
 
@@ -65,6 +120,10 @@ document.addEventListener('DOMContentLoaded', function() {
         alpacaOverlay.addEventListener('click', function(event) {
             if (event.target === alpacaOverlay) {
                 alpacaOverlay.style.display = 'none';
+<<<<<<< HEAD
+=======
+                body.classList.remove('modal-open'); // Enable body scroll
+>>>>>>> 5733b86 (initial commit from surface)
             }
         });
     }
@@ -75,13 +134,21 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
+<<<<<<< HEAD
+=======
+    // Toggle mobile nav
+>>>>>>> 5733b86 (initial commit from surface)
     hamburgerMenu.addEventListener('click', () => {
         if (mobileNav.classList.contains('open')) {
             mobileNav.classList.add('close');
             setTimeout(() => {
                 mobileNav.classList.remove('open');
                 mobileNav.classList.remove('close');
+<<<<<<< HEAD
             }, 300); // Match this duration to your CSS transition duration
+=======
+            }, 300);
+>>>>>>> 5733b86 (initial commit from surface)
         } else {
             mobileNav.classList.add('open');
         }
@@ -101,7 +168,11 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
     
+<<<<<<< HEAD
     // Initial check
+=======
+    // Initial check for hamburger menu visibility
+>>>>>>> 5733b86 (initial commit from surface)
     if (window.innerWidth > 768) {
         hamburgerMenu.style.display = 'none';
     } else {
@@ -110,14 +181,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     window.onscroll = function() {
         const header = document.querySelector('header');
+<<<<<<< HEAD
         const contactLink = document.querySelector('nav ul li a[href="#contact"]');
         const resumeLink = document.querySelector('nav ul li a[href="#resume"]');
+=======
+>>>>>>> 5733b86 (initial commit from surface)
 
         if (window.scrollY > 50) {
             header.classList.add('scrolled');
         } else {
             header.classList.remove('scrolled');
         }
+<<<<<<< HEAD
 
         // Detect if the user is at the bottom of the page
         if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 2) {
@@ -130,13 +205,35 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     };
 
+=======
+    };
+
+    // Intersection Observer to detect when the footer is in view
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                contactLink.classList.add('active');
+            } else {
+                contactLink.classList.remove('active');
+            }
+        });
+    });
+
+    // Start observing the footer element
+    observer.observe(footer);
+
+>>>>>>> 5733b86 (initial commit from surface)
     document.querySelectorAll('.accordion-item').forEach(item => {
         item.addEventListener('click', () => {
             item.classList.toggle('active');
         });
     });
 
+<<<<<<< HEAD
     // Preload images when the DOM is ready
+=======
+    // Preload images
+>>>>>>> 5733b86 (initial commit from surface)
     const images = document.querySelectorAll('.collage img');
     images.forEach(img => {
         const src = img.getAttribute('src');
@@ -144,6 +241,30 @@ document.addEventListener('DOMContentLoaded', function() {
         imgPreload.src = src;
     });
 
+<<<<<<< HEAD
+=======
+    // Open PDF in full-screen modal
+    viewResumeBtn.addEventListener('click', function(event) {
+        event.preventDefault();
+        pdfModal.style.display = 'flex';
+        body.classList.add('modal-open'); // Disable body scroll
+    });
+
+    // Close the PDF modal
+    pdfCloseBtn.addEventListener('click', function() {
+        pdfModal.style.display = 'none';
+        body.classList.remove('modal-open'); // Enable body scroll
+    });
+
+    // Close the PDF modal when clicking outside the content
+    pdfModal.addEventListener('click', function(event) {
+        if (event.target === pdfModal) {
+            pdfModal.style.display = 'none';
+            body.classList.remove('modal-open'); // Enable body scroll
+        }
+    });
+
+>>>>>>> 5733b86 (initial commit from surface)
     // Navbar link highlighting based on scroll position
     window.addEventListener('scroll', () => {
         let sections = document.querySelectorAll('section');
@@ -165,4 +286,8 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+<<<<<<< HEAD
 });
+=======
+});
+>>>>>>> 5733b86 (initial commit from surface)
